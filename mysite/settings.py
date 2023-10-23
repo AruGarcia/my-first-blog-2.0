b860196e2012baedc40036b68d98ee390bcb9313
Aruanã Garcia <aruanagarcia@gmail.com>
2023-10-23 09:21:34 -0300

    Add model for post in the blog and make tests for Post.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.base'
+    'mysite.base',
+    'mysite.blog'
 ]
 
 MIDDLEWARE = [
