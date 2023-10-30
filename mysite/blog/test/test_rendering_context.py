58e7cb2e93024710889be3f8a282aae05a7b447f
Aruanã Garcia <aruanagarcia@gmail.com>
2023-10-29 23:35:53 -0300

    Teste de renderização e contexto do blog post_list.

---
+++ mysite/blog/test/test_rendering_context.py
@@ -0,0 +1,51 @@
+import pytest
+from django.template import Template, Context
+from datetime import datetime
+from mysite.blog.models import Post  # Substitua 'mysite.blog' pelo nome do seu aplicativo
+
+
+@pytest.fixture
+def posts():
+    return [
+        Post(title='Post 1', published_date=datetime(2023, 10, 23), text='Texto do Post 1'),
+        Post(title='Post 2', published_date=datetime(2023, 10, 24), text='Texto do Post 2'),
+    ]
+
+
+def format_date(date):
+    return date.strftime("%d de %B de %Y às %H:%M")
+
+
+def test_render_posts_template(posts):
+    template_code = """
+    {% for post in posts %}
+        <article>
+            <time>published: {{ post.published_date }}</time>
+            <h2><a href="">{{ post.title }}</a></h2>
+            <p>{{ post.text|linebreaksbr }}</p>
+        </article>
+    {% endfor %}
+    """
+
+    template = Template(template_code)
+
+    # Formate as datas no mesmo formato esperado pelo template
+    formatted_posts = [
+        {
+            'title': post.title,
+            'published_date': format_date(post.published_date),
+            'text': post.text
+        }
+        for post in posts
+    ]
+
+    context = Context({'posts': formatted_posts})
+
+    rendered_template = template.render(context)
+
+    print(rendered_template)
+
+    for post in formatted_posts:
+        assert f"published: {post['published_date']}" in rendered_template
+        assert f"<h2><a href=\"\">{post['title']}</a></h2>" in rendered_template
+        assert f"<p>{post['text']}</p>" in rendered_template
---
+++ mysite/blog/test/test_rendering_context_II.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from mysite.blog.models import Post
+
+
+@pytest.fixture
+def client():
+    from django.test import Client
+    return Client()
+
+
+def test_blog_post_render(client, db):
+    user = User.objects.create(username="testuser")
+
+    # Create a sample blog post
+    Post.objects.create(
+        author=user,
+        title="Test Blog Post",
+        text="This is the content of the test blog post.",
+        published_date=datetime(2023, 5, 24)
+
+    )
+
+    url = reverse('blog:post_list')  # Replace 'blog_post' with your actual URL name
+    response = client.get(url)
+
+    assert response.status_code == 200
+    # assert b"User" in response.content
+    assert b"Test Blog Post" in response.content  # Check for the post title in the response
+    assert b"This is the content of the test blog post." in response.content  # Check for the post content
+    assert b"24 de Maio de 2023" in response.content  # Check for the post author
